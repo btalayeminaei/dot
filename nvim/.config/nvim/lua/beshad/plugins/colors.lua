6c35d0d6712bf8685dc14d583fe9d6ac7d65ab91
Beshad Talayeminaei <btalayeminaei@gmail.com>
2024-01-23 13:55:31 -0500

    reorg lazy plugins

---
+++ nvim/.config/nvim/lua/beshad/plugins/colors.lua
@@ -0,0 +1,10 @@
+return {
+    -- 'folke/tokyonight.nvim',
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    }
+}
