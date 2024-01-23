fd715ca35ccd97e36bae9d531afa16463658674b
Beshad Talayeminaei <btalayeminaei@gmail.com>
2024-01-23 07:44:12 -0500

    reorg lazy nvim

---
+++ nvim/.config/nvim/lua/beshad/lazy/init.lua
@@ -0,0 +1,14 @@
+return {
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  'github/copilot.vim',
+  -- treesitter
+  -- telescope
+  -- LSP
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+}
