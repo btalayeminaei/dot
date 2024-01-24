ffcabbfdfc8e807ac821318d89dc3eec7fd8b603
Beshad Talayeminaei <btalayeminaei@gmail.com>
2024-01-23 22:03:36 -0500

    Add vim fugitive

--- nvim/.config/nvim/lua/beshad/lazy_init.lua
+++ nvim/.config/nvim/lua/beshad/lazy_init.lua
@@ -11,4 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("beshad.plugins")
+require("lazy").setup({
+  spec = "beshad.plugins",
+  change_detection = { notify = false }
+})
