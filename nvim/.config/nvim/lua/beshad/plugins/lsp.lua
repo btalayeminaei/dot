d4bbeacb2f97b093f8c72f1412c495aa5efa6230
Beshad Talayeminaei <btalayeminaei@gmail.com>
2024-01-24 22:11:57 -0500

    redo lsp

---
+++ nvim/.config/nvim/lua/beshad/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "tsserver" }
+    })
+
+    -- After setting up mason-lspconfig you may set up servers via lspconfig
+    require('lspconfig').lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {
+              'vim',
+            },
+          },
+        },
+      },
+    }
+    require('lspconfig').tsserver.setup {}
+  end
+}
--- nvim/.config/nvim/lua/beshad/remap.lua
+++ nvim/.config/nvim/lua/beshad/remap.lua
@@ -12,3 +12,24 @@ vim.keymap.set("n", "<leader>-", ":sp<CR>")
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(e)
+    -- Enable completion triggered by <c-x><c-o>
+    --vim.bo[e.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = e.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
