8459039d91425e1b0e33295d3784083e6823bd7e
Beshad Talayeminaei <btalayeminaei@gmail.com>
2025-06-26 10:34:58 -0400

    restructure dotfiles

--- install.sh
+++ install.sh
@@ -1,47 +1,9 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 eval $(/opt/homebrew/bin/brew shellenv)
+
 brew update
 brew upgrade
 
-# consider using brew bundle
-# brew install git
-# brew install neovim
-# brew install ripgrep
-# brew install fd
-# brew install wget
-# brew install stow
-# brew install libpq
-# brew install tmux
-#
-# brew install rbenv
-# eval "$(rbenv init - zsh)"
-# rbenv install 3.2.2
-# rbenv global 3.2.2
-#
-# brew install nodenv
-# eval "$(nodenv init -)"
-# nodenv install 20.10.0
-# nodenv global 20.10.0
-#
-# brew install pyenv
-# eval "$(pyenv init -)"
-#
-# brew install direnv
-#
-# brew install cask
-# brew install --cask iterm2
-# brew install --cask google-chrome
-# brew install --cask rectangle
-# brew install --cask docker
-# brew install --cask gimp
-#
-# brew tap heroku/brew
-# brew install heroku
-
-
-stow zsh
-stow nvim
-stow stuff
+stow home
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
--- nvim/.config/nvim/lua/beshad/lazy_uninstall.sh
+++
@@ -1,1 +0,0 @@
-rm -rf ~/.local/state/nvim/lazy && rm -rf ~/.local/share/nvim/lazy && rm $DOTFILES/nvim/.config/nvim/lazy-lock.json
